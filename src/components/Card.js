77784d474d8f99dcde11c6e4fd89abfc6c17a03a
Carlstein <sara.carlstein@capgemini.com>
2021-11-30 10:45:39 +0100

    flip card added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,62 +1,60 @@
-import { apply } from 'file-loader'
-import React, { useEffect, useState } from 'react'
-var Snow = require('react-snow-effect')
-import ReactCardFlip from 'react-card-flip'
-import '../app.css'
+import { apply } from 'file-loader';
+import React, { useEffect, useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
+import "../app.css"
 
 const Card = () => {
-    const [data, setData] = useState([{ text: 'Loading...' }])
-    const [isFlipped, setIsFlipped] = React.useState(false)
+    const [data, setData] = useState([{ text: "Loading..." }])
+    const [isFlipped, setIsFlipped] = useState(new Set())
 
     useEffect(() => {
         fetch('https://rawgit.com/elijahmanor/cyberpun/master/jokes.json')
-            .then((res) => res.json())
-            .then((data) => setData(data))
+            .then(res => res.json())
+            .then(data => setData(data))
+
     }, [])
 
     const range = (upTo) => {
-        let array = []
+        let array = [];
         for (let i = 1; i < upTo; i++) {
             array.push(i)
         }
         return array
     }
 
+    useEffect(() => {
+
+
+    }, [])
+
+    function handleClick(id) {
+        console.log("Handle Click ", id)
+        return (e) => {
+            e.preventDefault();
+            let flipped = new Set(isFlipped);
+            if (flipped.has(id)) {
+                flipped.delete(id);
+            } else {
+                flipped.add(id);
+            }
+            setIsFlipped(flipped);
+        };
+    }
+
     const renderButtons = () => {
-        return range(25).map((number) => {
-            console.log(number)
-            return (
-                <>
-                    <ReactCardFlip
-                        isFlipped={isFlipped}
-                        flipDirection="horizontal"
-                    >
-                        <button
-                            className="card mountains-of-christmas"
-                            onMouseEnter={() => setIsFlipped((prev) => !prev)}
-                        >
-                            {number}
-                        </button>
-                        <button
-                            className="card"
-                            onMouseLeave={() => setIsFlipped((prev) => !prev)}
-                        >
-                            {' '}
-                            {data[number]?.text}
-                        </button>
-                    </ReactCardFlip>
-                </>
-            )
+        return range(25).map(number => {
+            return <ReactCardFlip isFlipped={isFlipped.has(number)} flipDirection="horizontal">
+                <button className="card" onClick={handleClick(number)}>{number}
+                </button>
+                <button className="card" onClick={handleClick(number)}> {data[number]?.text}</button>
+            </ReactCardFlip>
         })
     }
 
     return (
-        <>
-            <div className="background">{renderButtons()}</div>
-            <div>
-                <Snow />
-            </div>
-        </>
+        <div className="background">
+            {renderButtons()}
+        </div>
     )
 }
 
